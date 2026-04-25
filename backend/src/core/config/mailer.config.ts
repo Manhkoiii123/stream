c391467062c30076a407892df7ad64bbcf24d403
manhtranduc <manhtranduc0202@gmail.com>
2026-04-25 10:46:36 +0700

    setup mailer

---
+++ backend/src/core/config/mailer.config.ts
@@ -0,0 +1,21 @@
+import type { MailerOptions } from '@nestjs-modules/mailer'
+import { ConfigService } from '@nestjs/config'
+
+export const getMailerConfig = (
+	configService: ConfigService
+): MailerOptions => {
+	return {
+		transport: {
+			host: configService.getOrThrow<string>('MAILER_HOST'),
+			port: configService.getOrThrow<number>('MAILER_PORT'),
+			secure: false,
+			auth: {
+				user: configService.getOrThrow<string>('MAILER_LOGIN'),
+				pass: configService.getOrThrow<string>('MAILER_PASS')
+			}
+		},
+		defaults: {
+			from: `"Stream" <${configService.getOrThrow<string>('MAILER_LOGIN')}>`
+		}
+	}
+}
