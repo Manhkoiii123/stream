81cd51147fcbead248f82e060e1c79b90ff71879
manhtranduc <manhtranduc0202@gmail.com>
2026-04-27 21:56:37 +0700

    init sponsorship

--- backend/src/core/core.module.ts
+++ backend/src/core/core.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config'
 import { GraphQLModule } from '@nestjs/graphql'
 
 import { getLiveKitConfig } from '../core/config/livekit.config'
+import { getStripeConfig } from '../core/config/stripe.config'
 import { AccountModule } from '../modules/auth/account/account.module'
 import { DeactivateModule } from '../modules/auth/deactivate/deactivate.module'
 import { PasswordRecoveryModule } from '../modules/auth/password-recovery/password-recovery.module'
@@ -19,6 +20,7 @@ import { FollowModule } from '../modules/follow/follow.module'
 import { CloudinaryModule } from '../modules/libs/cloudinary/cloudinary.module'
 import { LivekitModule } from '../modules/libs/livekit/livekit.module'
 import { MailModule } from '../modules/libs/mail/mail.module'
+import { StripeModule } from '../modules/libs/stripe/stripe.module'
 import { TelegramModule } from '../modules/libs/telegram/telegram.module'
 import { NotificationModule } from '../modules/notification/notification.module'
 import { IngressModule } from '../modules/stream/ingress/ingress.module'
@@ -58,6 +60,11 @@ import { RedisModule } from './redis/redis.module'
 		LivekitModule.registerAsync({
 			imports: [ConfigModule],
 			useFactory: getLiveKitConfig,
+			inject: [ConfigService]
+		}),
+		StripeModule.registerAsync({
+			imports: [ConfigModule],
+			useFactory: getStripeConfig,
 			inject: [ConfigService]
 		}),
 		IngressModule,
